2539c2e3bf1914bb0c818dcbafea390b2730738c
Naum <naum.khenkin@bk.ru>
2024-10-31 16:03:07 +0200

    фыв

--- bot.py
+++ bot.py
@@ -18,6 +18,7 @@ dp = Dispatcher(bot, storage=storage)
 async def db_connect():
     return await aiosqlite.connect('case2giga.db')
 
+reagents = ['маршрут', 'путь', 'маршут', 'мршрут', 'маршрт', 'мршрт', 'маршрyт', '']
 
 async def get_places_from_db():
     db = await db_connect()
@@ -25,60 +26,64 @@ async def get_places_from_db():
     try:
         async with db.execute("SELECT name, average_time FROM giga") as cursor:
             async for row in cursor:
-                name = row[0].strip().lower() 
-                average_time = row[1]  
-                places[name] = average_time  
-                print(f"Загружено место: {name}, время: {average_time}") 
+                name = row[0].strip().lower()
+                average_time = row[1]
+                places[name] = average_time
+                print(f"Загружено место: {name}, время: {average_time}")
     finally:
         await db.close()
     return places
 
 def extract_days(user_query: str) -> int:
-    match = re.search(r'на (\d+) (дня|день)', user_query)
-    return int(match.group(1)) if match else 1 
+    match = re.search(r'на (\d+) (дня|день|дней)', user_query)
+    return int(match.group(1)) if match else 1
 
 async def create_itinerary(days: int, preferences: list) -> str:
+    print(days)
     if not preferences:
         return "Нет доступных мероприятий."
-    
+
+    total_hours_per_day = 12 
+    used_places = set()  
     itinerary = []
-    total_hours_per_day = 12  # Общее время на каждый день
-    used_places = set()  # Множество для хранения использованных мест
+    num_places = len(preferences)  
+    places_per_day = max(1, num_places // days - 1)  
 
-    # Если количество мест меньше, чем количество дней, возвращаем ошибку
-    if len(preferences) < days:
-        return "Недостаточно уникальных мест для планирования всех дней."
+    random.shuffle(preferences)
 
+    print(preferences)
     for day in range(days):
         available_time = total_hours_per_day
         daily_places = []
-        
-       
-        available_preferences = [place for place in preferences if place[0] not in used_places]
-        
-        
-        random.shuffle(available_preferences)
+        daily_count = 0  
 
-        for place_name, average_time in available_preferences:
-            average_time_float = float(average_time)
+        for place_name, average_time in preferences:
+            if daily_count >= places_per_day:  
+                break
+            if place_name not in used_places:  
+                average_time_float = float(average_time)
 
-            if available_time >= average_time_float:  
-                daily_places.append((place_name, average_time_float))
-                used_places.add(place_name)  
-                available_time -= average_time_float
-        
+                if available_time >= average_time_float:  
+                    daily_places.append((place_name, average_time_float))
+                    used_places.add(place_name)  
+                    available_time -= average_time_float
+                    daily_count += 1 
+
         if daily_places:
-
             day_itinerary = ', '.join([f"{place[0]} ({place[1]}ч)" for place in daily_places])
             daily_total_time = sum(place[1] for place in daily_places)
-            itinerary.append(f"День {day + 1}: {day_itinerary} - Общее время: {daily_total_time:.1f}ч")
+            
+            itinerary.append(f"🌟 **День {day + 1}** 🌟\n\n"
+                 f"🗺️ **Места для посещения:**\n"
+                 f"{day_itinerary}\n\n"
+                 f"⏰ **Общее время**: {daily_total_time:.1f} ч\n\n")
         else:
             itinerary.append(f"День {day + 1}: Нет доступных мероприятий.")
 
     return "\n".join(itinerary)
 
 def get_access_token():
-    master_token = "YmZlNGMwYWQtM2E0ZS00NzQ3LWIzMzQtZWYxN2NjNTYxODEyOmZjOWQ0ZDNlLTlhMzctNGRiMi1iNTVmLTMzMjYwNmI2MzBjZg=="  # Задайте ваш токен здесь
+    master_token = "YmZlNGMwYWQtM2E0ZS00NzQ3LWIzMzQtZWYxN2NjNTYxODEyOmZjOWQ0ZDNlLTlhMzctNGRiMi1iNTVmLTMzMjYwNmI2MzBjZg=="
     headers = {
         'Authorization': f'Bearer {master_token}',
         'RqUID': str(uuid.uuid4()),
@@ -98,7 +103,7 @@ def get_access_token():
 async def get_gigachat_response(query: str) -> list:
     access_token = get_access_token()
     if not access_token:
-        return []  # Ошибка получения токена
+        return []
 
     payload = json.dumps({
         "model": "GigaChat",
@@ -121,7 +126,7 @@ async def get_gigachat_response(query: str) -> list:
                 print(f"Ответ от GigaChat: {content}")
 
                 database_places = await get_places_from_db()
-                preferences = extract_places_from_text(content, database_places)  
+                preferences = extract_places_from_text(content, database_places)
 
                 print(f"Извлеченные предпочтения: {preferences}")
                 return preferences
@@ -129,19 +134,46 @@ async def get_gigachat_response(query: str) -> list:
                 logging.error("Ошибка запроса к GigaChat: %s", await response.text())
                 return []
 
+
+async def get_message_from_gigachat(query: str) -> list:
+    access_token = get_access_token()
+    if not access_token:
+        return []
+
+    payload = json.dumps({
+        "model": "GigaChat",
+        "messages": [{"role": "user", "content": query}],
+        "stream": False,
+        "repetition_penalty": 1
+    })
+
+    headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': f'Bearer {access_token}'
+    }
+
+    async with aiohttp.ClientSession() as session:
+        async with session.post(GIGACHAT_API_URL, headers=headers, data=payload, ssl=False) as response:
+            if response.status == 200:
+                json_response = await response.json()
+                content = json_response['choices'][0]['message']['content']
+                print(f"Ответ от GigaChat: {content}")                
+                return content
+            else:
+                logging.error("Ошибка запроса к GigaChat: %s", await response.text())
+                return []
+
 def extract_places_from_text(text: str, database_places: dict) -> list:
     found_places = []
-    
 
     print(f"Текст для обработки: {text}")
 
     lines = text.splitlines()
 
-
     for line in lines:
-        line = line.strip() 
+        line = line.strip()
         if len(line) > 0:
-
             for place_name in database_places.keys():
                 if place_name in line.lower():  
                     found_places.append((place_name, str(database_places[place_name])))
@@ -149,22 +181,15 @@ def extract_places_from_text(text: str, database_places: dict) -> list:
                     break  
 
     return found_places
+
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
     await message.reply("""
 Здравствуйте! 🌟
-
 Я здесь, чтобы помочь вам создать незабываемое путешествие! ✈️ Чтобы подобрать оптимальное количество дней для вашей поездки и предложить вам самые интересные места для посещения, мне нужно несколько деталей:
-
 🌍 Куда вы планируете поехать?  
-Каждое направление уникально, и я хочу знать, куда поведет вас ваше сердечное желание!
-
 💖 Какие у вас интересы?  
-Предпочитаете ли вы культурный отдых с погружением в историю, восхитительные природные пейзажи, или, возможно, гастрономические приключения, где вы сможете попробовать локальные деликатесы? Дайте знать, что вас вдохновляет!
-
 🎉 Есть ли какие-то конкретные достопримечательности или мероприятия, которые вы мечтаете посетить?  
-Будь то знаменитый музей, уютное кафе с потрясающими десертами или захватывающее событие, я помогу вам включить это в ваш план!
-
 Ваши предпочтения позволят мне предложить наиболее подходящие варианты для вашего путешествия. Давайте создадим вместе ваше идеальное приключение! 🌅✨
 """)
 
@@ -179,21 +204,24 @@ async def get_items(message: types.Message):
     finally:
         await db.close()
 
-
-
 @dp.message_handler(content_types=['text'])
 async def handle_text(message: types.Message):
-
     user_query = message.text
-    
-    days = extract_days(user_query)
+    if not any(reagent in user_query for reagent in reagents):
+        response = await get_message_from_gigachat(user_query)
 
-    preferences = await get_gigachat_response(user_query)
-    itinerary = await create_itinerary(days, preferences)
-    await message.answer(itinerary)
-
-    
-
+        if response:
+            await message.answer(response)
+        else:
+            days = extract_days(user_query)
+            preferences = await get_gigachat_response(user_query)
+            itinerary = await create_itinerary(days, preferences)
+            await message.answer(itinerary)
+    else:
+        days = extract_days(user_query)
+        preferences = await get_gigachat_response(user_query)
+        itinerary = await create_itinerary(days, preferences)
+        await message.answer(itinerary)
 
 if __name__ == '__main__':
     print(banner)
